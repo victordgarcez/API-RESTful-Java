37f67a5c0ecc74ca138bdb32227f1ea846a5619b
victordgarcez <contatovictorhdg@gmail.com>
2025-07-28 19:19:55 -0300

    Initial commit

---
+++ src/main/java/victordgarcez/api_rest_cloud/API/ApiResTfulJavaApplication.java
@@ -0,0 +1,13 @@
+package victordgarcez.api_rest_cloud.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiResTfulJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiResTfulJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/victordgarcez/api_rest_cloud/API/ApiResTfulJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package victordgarcez.api_rest_cloud.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiResTfulJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
